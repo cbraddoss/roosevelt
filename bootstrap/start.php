e310c3c5690c87e3198d943ccf18fdfe3d860701
C. Brad Doss <cbraddoss@gmail.com>
2014-03-24 18:59:21 -0500

    added my laptop as dev environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,6 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'dev-brad' => array('Cs-Mac-Pro.local'),
+	'dev-brad-laptop' => array('Brad-Dosss-MacBook-Pro.local'),
 	'dev-jack' => array('CHANGE_ME'),
 	'dev-taylor' => array('CHANGE_ME'),
 	'production' => array('roosevelt.insideout.com')
