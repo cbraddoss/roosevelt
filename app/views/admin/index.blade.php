9a46e7a899b468a7003c7d8e39ebfce1c18e4892
C. Brad Doss <cbraddoss@Cs-Mac-Pro.local>
2014-03-24 16:40:45 -0500

    admin page user management styling tweaks.

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -9,50 +9,54 @@
 		<h3>User Management</h3>
 		<table id="users-table">
 			<tr>
-				<th>Name</th><th>Email</th><th>Username</th><th>Password</th><th>User Role</th><th>Extension</th><th>Cell Phone</th><th>Status</th><th>Edit</th>
+				<th class="title-name">Name</th>
+				<th class="title-email">Email</th>
+				<th class="title-password">Password</th>
+				<th class="title-userrole">User Role</th>
+				<th class="title-extension">Extension</th>
+				<th class="title-cell-phone">Cell Phone</th>
+				<th class="title-status">Status</th>
+				<th class="title-edit">Edit</th>
 			</tr>
 		@foreach($users as $u)
 			<tr class="user-list user-list-{{ $u->id }}">
-				<td>{{ $u->first_name }} {{ $u->last_name }}</td>
-				<td>{{ $u->email }}</td>
-				<td>{{ $u->username }}</td>
-				<td>********</td>
-				<td>{{ $u->userrole }}</td>
-				<td>{{ $u->extension }}</td>
-				<td>{{ $u->cell_phone }}</td>
-				<td>@if( $u->active == 1) <span class="ss-check"></span> @else <span class="ss-delete"></span> @endif</td>
-				<td><button id="{{ $u->id }}" class="edit">Edit</button></td>
+				<td class="user-name">{{ $u->first_name }} {{ $u->last_name }}</td>
+				<td class="user-email">{{ $u->email }}</td>
+				<td class="user-password">********</td>
+				<td class="user-userrole">{{ $u->userrole }}</td>
+				<td class="user-extension">{{ $u->extension }}</td>
+				<td class="user-cell-phone">{{ $u->cell_phone }}</td>
+				<td class="user-active">@if( $u->active == 1) <span class="ss-check"></span> @else <span class="ss-delete"></span> @endif</td>
+				<td class="user-edit"><button id="{{ $u->id }}" class="edit ss-write"></button></td>
 			</tr>
 			
 			<tr id="user-{{ $u->id }}" class="user-form">
-				<td colspan="9">
+				<td colspan="8">
 					<div class="user-update-form">
-					<h4>Edit: {{ $u->first_name }} {{ $u->last_name }}</h4>
-					{{ Form::open( array('id' => '$u->id') ) }}
-					<div class="left">
+					
+					{{ Form::open( array('id' => $u->id) ) }}
+					
 					{{ Form::hidden('id',$u->id) }}
-					{{ Form::label('first_name','First Name:') }}
-					{{ Form::text('first_name', $u->first_name, array('placeholder' => 'First Name', 'class' => 'field', 'autofocus' => 'autofocus')) }}
-					{{ Form::label('last_name','Last Name:') }}
-					{{ Form::text('last_name', $u->last_name, array('placeholder' => 'Last Name', 'class' => 'field')) }}
-					{{ Form::label('email','Email:') }}
-					{{ Form::email('email', $u->email, array('placeholder' => 'Email Address', 'class' => 'field')) }}
-					</div>
-					{{ Form::label('username','Username:') }}
-					{{ Form::text('username', $u->username, array('placeholder' => 'Username', 'class' => 'field')) }}
-					{{ Form::label('password','Password:') }}
-					{{ Form::password('password', null, array('placeholder' => 'Password', 'class' => 'field')) }}
-					{{ Form::label('userrole','User Role:') }}
-					{{ Form::text('userrole', $u->userrole, array('placeholder' => 'User Role', 'class' => 'field')) }}
-					{{ Form::label('extension','Extension:') }}
-					{{ Form::text('extension', $u->extension, array('placeholder' => 'Extension', 'class' => 'field')) }}
-					{{ Form::label('cell_phone','Cell:') }}
-					{{ Form::text('cell_phone', $u->cell_phone, array('placeholder' => 'Cell Phone', 'class' => 'field')) }}
-					{{ Form::label('active','Status:') }}
-					{{ Form::checkbox('active', $u->active ) }}
+					
+					{{ Form::text('first_name', $u->first_name, array('placeholder' => 'First Name', 'class' => 'first-name field', 'autofocus' => 'autofocus')) }}
+					
+					{{ Form::text('last_name', $u->last_name, array('placeholder' => 'Last Name', 'class' => 'last-name field')) }}
+					
+					{{ Form::email('email', $u->email, array('placeholder' => 'Email Address', 'class' => 'email field')) }}
+					
+					{{ Form::password('password', null, array('placeholder' => 'Password', 'class' => 'password field')) }}
+					
+					{{ Form::text('userrole', $u->userrole, array('placeholder' => 'User Role', 'class' => 'userrole field')) }}
+					
+					{{ Form::text('extension', $u->extension, array('placeholder' => 'Extension', 'class' => 'extension field')) }}
+					
+					{{ Form::text('cell_phone', $u->cell_phone, array('placeholder' => 'Cell Phone', 'class' => 'cell-phone field')) }}
+					
+					{{ Form::checkbox('active', $u->active , array('class' => 'active checkbox')) }}
 					{{ Form::close() }}
+					<button id="{{ $u->id }}" class="save">Save</button>
 					<button id="{{ $u->id }}" class="cancel">Cancel</button>
-					<button id="{{ $u->id }}" class="delete">Delete</button>
+					<button id="{{ $u->id }}" class="delete">Delete User</button>
 					</div>
 				</td>
 			</tr>
--- public/js/office.js
+++ public/js/office.js
@@ -20,34 +20,46 @@ jQuery(document).ready(function($){
 	$('#users-table button.edit').click(function(){
 		$('#users-table').find('button.edit').each(function() {
 			$(this).attr('disabled', true);
+			$(this).css('cursor', 'not-allowed');
 		});
 		$('#users-table .user-form').hide();
-		$userToEdit = $(this).attr('id');
+		var userRow = $(this).attr('id');
 		//alert($userToEdit);
-		$(this).parent().parent().find('td').fadeTo("fast",0.5).css({
-			'background':'#ddd',
-			'border-bottom':'none',
+		$(this).parent().parent().find('td').hide();
+		$('#users-table').find('.user-list').each(function() {
+			$(this).fadeTo("slow",0.5);
+			$(this).css('cursor', 'not-allowed');
 		});
-		$(this).parent().parent().find('td').first().css({
-			'border-left':'1px solid rgba(32,32,32,0.2)'
-		});
-		$(this).parent().parent().find('td').last().css({
-			'border-right':'1px solid rgba(32,32,32,0.2)'
-		});
-		$(this).parent().parent().parent().find('#user-'+$userToEdit).toggle();
+		$(this).parent().parent().parent().find('#user-'+userRow).fadeTo("slow",1);
+		var userNameWidth = $(this).parent().parent().parent().find('.title-name').width();
+		var userEmailWidth = $(this).parent().parent().parent().find('.title-email').width();
+		var userPasswordWidth = $(this).parent().parent().parent().find('.title-password').width();
+		var userUserroleWidth = $(this).parent().parent().parent().find('.title-userrole').width();
+		var userExtensionWidth = $(this).parent().parent().parent().find('.title-extension').width();
+		var userCellPhoneWidth = $(this).parent().parent().parent().find('.title-cell-phone').width();
+		var userStatusWidth = $(this).parent().parent().parent().find('.title-status').width();
+		//alert(userNameWidth);
+		$(this).parent().parent().parent().find('#user-'+userRow+' input.first-name').css('width',((userNameWidth)/2)-6+'px');
+		$(this).parent().parent().parent().find('#user-'+userRow+' input.last-name').css('width',((userNameWidth)/2)-6+'px');
+		$(this).parent().parent().parent().find('#user-'+userRow+' input.email').css('width',(userEmailWidth-4)+'px');
+		$(this).parent().parent().parent().find('#user-'+userRow+' input[name="password"]').css('width',(userPasswordWidth-4)+'px');
+		$(this).parent().parent().parent().find('#user-'+userRow+' input.userrole').css('width',(userUserroleWidth-4)+'px');
+		$(this).parent().parent().parent().find('#user-'+userRow+' input.extension').css('width',(userExtensionWidth-4)+'px');
+		$(this).parent().parent().parent().find('#user-'+userRow+' input.cell-phone').css('width',(userCellPhoneWidth-4)+'px');
+		$(this).parent().parent().parent().find('#user-'+userRow+' input.status').css('width',(userStatusWidth-4)+'px');
 	});
 	$('#users-table button.cancel').click(function(){
 		$('#users-table').find('button.edit').each(function() {
 			$(this).attr('disabled', false);
+			$(this).css('cursor', 'pointer');
+		});
+		$('#users-table').find('.user-list').each(function() {
+			$(this).fadeTo("slow",1);
+			$(this).css('cursor', 'inherit');
 		});
 		$('#users-table .user-form').hide();
-		$userToCancel = $(this).attr('id');
-		$(this).parent().parent().parent().parent().find('.user-list-'+$userToCancel+' td').fadeTo("fast",1).css({
-			'background':'#fff',
-			'border-left':'none',
-			'border-right':'none',
-			'border-bottom':'1px solid rgba(32,32,32,0.1)'
-		});
+		var userRow = $(this).attr('id');
+		$(this).parent().parent().parent().parent().find('.user-list-'+userRow+' td').fadeTo("slow",1);
 	});
 	//Change color of high priority project items
 	$(document).on('change','.priority',function(){
