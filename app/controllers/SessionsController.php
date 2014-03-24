11ba3beb35352d10fa0d746aafca577dd10a0a84
C. Brad Doss <cbraddoss@Cs-Mac-Pro.local>
2014-03-24 15:28:18 -0500

    When viewing /login when already logged in, an error was received. This should fix that error.

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -14,7 +14,7 @@ class SessionsController extends \BaseController {
 		
 		// Redirect to Dashboard.
 		//Todo: redirect to intended() url
-		else return View::make('dashboard.index');
+		else return Redirect::route('dashboard');
 	}
 
 	/**
