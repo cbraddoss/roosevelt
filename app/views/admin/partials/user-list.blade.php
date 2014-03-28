628e1845a552946f3cecd99cdedb49da31c8c272
C. Brad Doss <cbraddoss@Cs-Mac-Pro.local>
2014-03-27 22:21:59 -0500

    Added new Last Login text below users on admin page. If no last login is available, set to 'Never'.

--- app/views/admin/partials/user-list.blade.php
+++ app/views/admin/partials/user-list.blade.php
@@ -1,4 +1,4 @@
-<td class="user-name" fieldvalfirst="{{ $u->first_name }}" fieldvallast="{{ $u->last_name }}"><div>{{ $u->first_name }} {{ $u->last_name }} <small class="last-login">@if($u->last_login != '0000-00-00 00:00:00') Last Login: {{ str_limit($u->last_login,10,'') }} @endif</small></div></td>
+<td class="user-name" fieldvalfirst="{{ $u->first_name }}" fieldvallast="{{ $u->last_name }}"><div>{{ $u->first_name }} {{ $u->last_name }} <small class="last-login">@if($u->last_login != '0000-00-00 00:00:00') Last Login: {{ str_limit($u->last_login,10,'') }} @else  Last Login: Never @endif</small></div></td>
 <td class="user-email" fieldval="{{ $u->email }}">{{ $u->email }}</td>
 <td class="user-password">********</td>
 <td class="user-userrole" fieldval="{{ $u->userrole }}">{{ $u->userrole }}</td>
