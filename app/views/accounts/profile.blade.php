a3ebc04fa6c4801a1d423525cfc859b578f6fa33
taylor_hasenpflug <taylor@insideout.com>
2014-03-31 15:44:08 -0700

    add structure for accounts and account profiles

---
+++ app/views/accounts/profile.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.main')
+
+@section('page-title')
+{{ 'Account - $account' }}
+@stop
+
+@section('page-content')
+<div id="page-title">
+	<h2>Account - {{ $account }}</h2>
+</div>
+
+<div class="inner-page">
+    
+</div>
+
+@stop
