1541d86da10dd2a1f9de972c25fb3e5b8dfe640c
C. Brad Doss <cbraddoss@Cs-Mac-Pro.local>
2014-04-30 01:44:08 -0500

    Mailer class contained a shortcut not available in php 5.3. commenting out function for now.

--- app/models/Mailer.php
+++ app/models/Mailer.php
@@ -2,7 +2,7 @@
 
 class Mailer {
 
-	public function sendTo($user, $subject, $view, $data = [])
+	public function sendTo($user, $subject, $view, $data = array())
     {
         Mail::queue($view, $data, function($message) use($user, $subject)
         {
@@ -11,23 +11,56 @@ class Mailer {
         });
     }
 
-     public function welcome(User $user)
-    {
-        $view = 'emails.welcome';
-        $data = [];
-        $subject = 'Welcome to Laracasts';
+    // public function article_ping(Article $article) {
 
-        return $this->sendTo($user, $subject, $view, $data);
-    }
 
-    public function cancellation(User $user)
-    {
-        $view = 'emails.cancel';
-        $data = [];
-        $subject = 'Sorry to see you go';
 
-        return $this->sendTo($user, $subject, $view, $data);
+    // 	$view = 'emails.ping';
+    // 	$data = [];
+    // 	$subject = 'IOS Remote Office Ping: ' . $article->title;
 
-    }
+    // 	return $this->sendTo($user, $subject, $view, $data);
+    // }
+
+ //    public function send_ping_email($newArticle) {
+	// 	$parseUsers = $newArticle->mentions;
+	// 	$parseUsers = explode(' ',$parseUsers);
+	// 	$users = array();
+	// 	foreach($parseUsers as $user) {
+	// 		if($user == '') unset($user);
+	// 		else {
+	// 			$users[] = $user;
+	// 		}
+	// 	}
+	// 	foreach($users as $user) {
+	// 		$userSend = User::where('user_path','=',$user)->first();
+	// 		//dd($userSend);
+	// 		$author = User::where('id', '=', $newArticle->author_id)->first();
+	// 		$pingDetails = array('title' => $newArticle->title, 'link' => 'http://roosevelt.insideout.com/news/article/'.$newArticle->link, 'author' => $author->first_name . ' ' . $author->last_name, 'created_at' => $newArticle->created_at);
+	// 		Mail::send('emails.ping', $pingDetails, function($message) use($userSend) {
+	// 			$message->from('office@insideout.com', 'InsideOut Employee Remote Office');
+	// 			$message->to($userSend->email, $userSend->first_name . ' ' . $userSend->last_name)->subject('You have been pinged!');
+	// 		});
+	// 	}
+	// }
+
+    //  public function welcome(User $user)
+    // {
+    //     $view = 'emails.welcome';
+    //     $data = [];
+    //     $subject = 'Welcome to Laracasts';
+
+    //     return $this->sendTo($user, $subject, $view, $data);
+    // }
+
+    // public function cancellation(User $user)
+    // {
+    //     $view = 'emails.cancel';
+    //     $data = [];
+    //     $subject = 'Sorry to see you go';
+
+    //     return $this->sendTo($user, $subject, $view, $data);
+
+    // }
 
 }
