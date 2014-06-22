295fcd219e5098df1431da2154915ee7eed53078
C. Brad Doss <cbraddoss@gmail.com>
2014-06-22 01:03:41 -0500

    Added IP address and Browser information to login reset form (for security purposes).

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -4,6 +4,10 @@
 <h2>Password Reset</h2>
 
 <div>
-	To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.
+	<p>To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.</p>
+	<p>
+	<small>This request was sent from the IP address <i>{{ $_SERVER['REMOTE_ADDR'] }}</i> using {{ $_SERVER['HTTP_USER_AGENT'] }}.</small>
+	<small>If this is not your IP address and/or the browser you are using, please notify the DevTeam.</small>
+	</p>
 </div>
 @stop
