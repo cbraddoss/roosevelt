716fd734d35d50e09248e6daa384fad1cb1f3913
C. Brad Doss <cbraddoss@gmail.com>
2014-11-07 14:56:14 -0600

    Changed: move filters on News list view page to a dropdown.

--- app/views/news/partials/news-menu.blade.php
+++ app/views/news/partials/news-menu.blade.php
@@ -14,19 +14,26 @@
 		<span> Date Filter</span>
 	</div>
 </li>
-<li class="select-dropdown right">
-	<span class="ss-dropdown"></span>
-	<span class="ss-directup"></span>
-	<select filterlink="/news/author/" class="filter-this filter-author news-filter">
-		<option value="0">Author Filter</option>
-		@if(!empty($userAuthor)) {{ get_user_list_select($userAuthor->first_name.' '.$userAuthor->last_name) }} @else {{ get_user_list_select() }} @endif
-	</select>
+<li class="right filter-show this-filter-show">
+	<span class="show-this-filter ss-navigatedown add-button"> Filters</span>
 </li>
-<li class="right select-dropdown">
-	<span class="ss-dropdown"></span>
-	<span class="ss-directup"></span>
-	<select filterlink="/news/tag/" class="filter-this filter-vault-tag tags-filter">
-		<option>Tag Filter</option>
-		{{ $articleTagsSelect }}
-	</select>
+<li class="page-menu-sub">
+	<ul>
+		<li class="select-dropdown right">
+			<span class="ss-dropdown"></span>
+			<span class="ss-directup"></span>
+			<select filterlink="/news/author/" class="filter-this filter-author news-filter">
+				<option value="0">Author Filter</option>
+				@if(!empty($userAuthor)) {{ get_user_list_select($userAuthor->first_name.' '.$userAuthor->last_name) }} @else {{ get_user_list_select() }} @endif
+			</select>
+		</li>
+		<li class="right select-dropdown">
+			<span class="ss-dropdown"></span>
+			<span class="ss-directup"></span>
+			<select filterlink="/news/tag/" class="filter-this filter-vault-tag tags-filter">
+				<option>Tag Filter</option>
+				{{ $articleTagsSelect }}
+			</select>
+		</li>
+	</ul>
 </li>
